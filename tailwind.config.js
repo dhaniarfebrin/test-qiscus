70fbda8a966d977978fefb117198b76aa8551935
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2024-06-14 12:03:36 +0700

    chore: color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#F8F4E1',
+        primary: '#543310',
+        secondary: '#74512D',
+        accent: '#AF8F6F'
+      }
+    },
     container: {
       center: true
-    },
-    colors: {
-      background: '#F8F4E1',
-      primary: '#543310',
-      secondary: '#74512D',
-      accent: '#AF8F6F'
     }
   },
   plugins: []
